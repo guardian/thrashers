0cb4c2f17404770ba7729d3c13fc62db870feed6
Sam Morris <mail@sammorrisdesign.co.uk>
2014-11-14 15:28:13 +0000

    ALL THE GRUNT

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,18 @@
-module.exports = function(grunt) {
+ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
+  var dir =  'embeds/' + (grunt.option('folderName') ? grunt.option('folderName') : '');
+  var scss = 'embeds/' + (grunt.option('folderName') ? grunt.option('folderName') + '/*.scss' : '**/*.scss');
+  var html = 'embeds/' + (grunt.option('folderName') ? grunt.option('folderName') + '/*.html' : '**/**/*.html'); 
+  var newDir = grunt.option('name');
+
   grunt.initConfig({
-    vars: {
-      dir: "embeds/" + grunt.option('folderName'),
-      newDir: grunt.option('name')
-    },
     watch: {
       css: {
-        files: ['<%= vars.dir %>/*.scss', '<%= vars.dir %>/*.html'],
-        tasks: ['sass']
+        files: [scss, html],
+        tasks: ['sass', 'compile']
       }
     },
     sass: {
@@ -19,9 +20,13 @@ module.exports = function(grunt) {
         options: {
           style: 'compressed'
         },
-        files: {
-            '<%= vars.dir %>/style.css' : '<%= vars.dir %>/style.scss'
-        }
+        files: [{
+            expand: true,
+            cwd: dir,
+            src: '**/*.scss',
+            dest: dir,
+            ext: '.css'
+        }]
       }
     },
     copy: {
@@ -36,20 +41,19 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.event.on('watch', function() {
-    var dir = grunt.option('folderName');
-    var fullDir = "embeds/" + dir;
-    var html = grunt.file.read(fullDir + "/index.html");
-    var css = grunt.file.read(fullDir + "/style.css");
-    var jsonFile = fullDir + "/source.json";
-
-    var project = grunt.file.readJSON(jsonFile);
-    project["html"] = '<div class="'+ dir +'__wrapper">' +
-                        '<style>' + css + '</style>' +
-                        html +
-                      '</div>';
-    grunt.file.write(jsonFile, JSON.stringify(project, null, 2));
+  grunt.registerTask('compile', function() {
+      grunt.file.expand({}, dir + '*').forEach(function(path) {
+        var html = grunt.file.read(path + "/index.html");
+        var css = grunt.file.read(path + "/style.css");
+        var jsonFile = path + "/source.json";
+        var localDir = path.split('/')[1];
+        var project = grunt.file.readJSON(jsonFile);
+        
+        project["html"] = '<div class="'+ localDir +'__wrapper">' + '<style>' + css + '</style>' + html + '</div>';
+        grunt.file.write(jsonFile, JSON.stringify(project, null, 2));
+      });
   });
 
-  grunt.registerTask('default', ['sass', 'watch']);
+  grunt.registerTask('new', ['copy']);
+  grunt.registerTask('default', ['sass', 'compile']);
 };
