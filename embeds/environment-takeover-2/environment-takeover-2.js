9396ccfa0d4f45b8d619b5f52528fe01829e3520
andymason <a.w.mason@gmail.com>
2015-04-29 18:19:33 +0100

    blah

--- embeds/environment-takeover-2/environment-takeover-2.js
+++ embeds/environment-takeover-2/environment-takeover-2.js
@@ -1,6 +1,5 @@
 (function() {
-
-  // Underscore's throttle.
+    // Underscore's throttle.
     var throttle = function(func, wait, options) {
         var context, args, result;
         var timeout = null;
@@ -31,34 +30,6 @@
         };
       };
 
-
-
-    function isAnimationSupported(elm) {
-        var animation = false,
-        animationstring = 'animation',
-        keyframeprefix = '',
-        domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
-        pfx  = '';
-        if( elm.style.animationName !== undefined ) { animation = true; }    
-        if( animation === false ) {
-          for( var i = 0; i < domPrefixes.length; i++ ) {
-            if( elm.style[ domPrefixes[i] + 'AnimationName' ] !== undefined ) {
-              pfx = domPrefixes[ i ];
-              animationstring = pfx + 'Animation';
-              keyframeprefix = '-' + pfx.toLowerCase() + '-';
-              animation = true;
-              break;
-            }
-          }
-        }
-        return animation;
-    }
-
-    var isIE = false;
-    if (navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > 0) {
-      isIE = true;
-    }
-
     function animate($, bean, cookies) {
         var COOKIE_NAME = '_thunder2';
         var cookieDays = 30;
@@ -69,18 +40,13 @@
           //return;
         }
 
-
         cookies.add(COOKIE_NAME, 1, cookieDays);
 
         var wrapperEl = $('.environment-takeover__overlay');
         if (!wrapperEl) { return; }
         wrapperEl.addClass('environment-takeover__active');
 
-        // IE has keyframe animation bugs, so skip to last frame
-        if (!isAnimationSupported(wrapperEl[0]) || isIE) {
-          wrapperEl.addClass('environment-takeover__no-animation');
-        }
-        
+
         var closeBtn = $('.environment-takeover__close-wrapper');
         bean.on(closeBtn[0], 'click', function() {
             wrapperEl.removeClass('environment-takeover__active');
