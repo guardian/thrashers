cd5c555cbd59e1f46c28b802563dcf99e5f95097
Sam Morris <mail@sammorrisdesign.co.uk>
2015-05-23 16:35:51 +0100

    Some tweaks

--- embeds/uni-guide/_source/main.js
+++ embeds/uni-guide/_source/main.js
@@ -1,10 +1,8 @@
-var peopleAnimEl = document.getElementsByClassName('ug16__anim--people')[0],
-    peopleRef = 0,
-    interval = 150;
+var peopleRef = 0;
 
 function peopleAnimation() {
     peopleRef = (peopleRef - 288) % 864;
-    peopleAnimEl.style.backgroundPosition = `center ${peopleRef}px`;
+    document.getElementsByClassName('ug16__anim--people')[0].style.backgroundPosition = `center ${peopleRef}px`;
 }
 
 window.setInterval(peopleAnimation, 150);
