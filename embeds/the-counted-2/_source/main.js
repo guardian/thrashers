553469a8c05846c076bc20472ec6833e443c8682
Sam Morris <mail@sammorrisdesign.co.uk>
2015-10-15 15:32:15 +0100

    animate with animate class

--- embeds/the-counted-2/_source/main.js
+++ embeds/the-counted-2/_source/main.js
@@ -1,3 +1,5 @@
 document.getElementsByClassName("the-counted__number--one")[0].className += " number--4";
 document.getElementsByClassName("the-counted__number--ten")[0].className += " number--3";
 document.getElementsByClassName("the-counted__number--hundred")[0].className += " number--8";
+
+document.getElementsByClassName("the-counted-2__wrapper")[0].className += " animate";
