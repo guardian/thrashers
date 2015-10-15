6358762912c9a20fee77b72662a23115c2fda4ad
Sam Morris <mail@sammorrisdesign.co.uk>
2015-10-15 15:20:27 +0100

    Animate the dails

---
+++ embeds/the-counted-2/_source/main.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("the-counted__number--one")[0].className += " number--4";
+document.getElementsByClassName("the-counted__number--ten")[0].className += " number--3";
+document.getElementsByClassName("the-counted__number--hundred")[0].className += " number--8";
