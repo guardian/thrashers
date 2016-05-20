ca9e3dd5b5f3361aafff55bc2e5a0e10d56f3c06
Duarte Carrilho da Graça <duarte@duarte.gd>
2016-05-20 17:08:30 +0100

    Game of Thrones thrasher

--- embeds/podcast/_source/main.js
+++ embeds/podcast/_source/main.js
@@ -22,33 +22,38 @@ loadJSON("http://interactive.guim.co.uk/docsdata-test/1rJsiJMlEqkSVbklFSKJBmwCYf
     document.getElementsByClassName("audio")[0].setAttribute("src", source);
     var audio = document.getElementsByClassName("audio")[0];
     var playButton = document.getElementsByClassName("player__play")[0];
-    var player = document.getElementsByClassName("player")[0];
+    var thrasher = document.getElementById("got-the-citadel-player-thrasher");
     document.getElementsByClassName("episode-info__title")[0].innerHTML = '<h1>' + name + '</h1>';
     var audioLength = 0;
     audio.oncanplaythrough = function(){
         audioLength = audio.duration;
-        //works out the time in minutes 
-        var floor = Math.floor(audioLength);
-        var minutes = audioLength / 60;
-        var minutesTen = minutes * 10;
-        var round = Math.round(minutesTen);
-        var time = round / 10;
-        document.getElementsByClassName("episode-info__time")[0].innerHTML = '<h2>' + time + '</h2>';
+        //works out the time in minutes
+        // var floor = Math.floor(audioLength);
+        // var minutes = audioLength / 60;
+        // var minutesTen = minutes * 10;
+        // var round = Math.round(minutesTen);
+        // var time = round / 10;
+        var mind = audioLength % (60 * 60);
+        var minutes = Math.floor(mind / 60);
+
+        var secd = mind % 60;
+        var seconds = Math.floor(secd);
+        document.getElementsByClassName("episode-info__time")[0].innerHTML = '<h2>' + minutes + ':' + seconds + '</h2>';
     }
     playButton.onclick = function(){
         if(audio.paused){
             audio.play();
-            player.setAttribute("data-playing", "playing")
+            thrasher.setAttribute("data-playing", "playing")
         }else{
             audio.pause();
-            player.removeAttribute("data-playing");
+            thrasher.removeAttribute("data-playing");
         }
         console.log('clicked');
     };
     audio.ontimeupdate = function(){
         console.log('length',audioLength);
         console.log('currentTime', Math.floor(audio.currentTime));
-        var timer = audio.currentTime / audioLength * 750; 
+        var timer = audio.currentTime / audioLength * 750;
         console.log('%', Math.floor(timer));
         document.getElementById("progress").setAttribute("stroke-dasharray", Math.floor(timer) + "," + "1000");
     }
