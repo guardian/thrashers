c1eb13cbb5e3132b318cb66952b63da3fbdf9852
Duarte Carrilho da Graça <duarte@duarte.gd>
2016-05-23 14:44:05 +0100

    created initial game of thrones thrasher

--- embeds/podcast/_source/main.js
+++ embeds/podcast/_source/main.js
@@ -18,12 +18,14 @@ function loadJSON(path, success, error) {
 
 loadJSON("http://interactive.guim.co.uk/docsdata-test/1rJsiJMlEqkSVbklFSKJBmwCYfuoS-JuhbDR1T7tNat0.json", function(data) {
     var  source =  data.sheets.Sheet1[0].Link;
+    var  episode =  data.sheets.Sheet1[0].Episode;
     var  name = data.sheets.Sheet1[0].Title;
     document.getElementsByClassName("audio")[0].setAttribute("src", source);
     var audio = document.getElementsByClassName("audio")[0];
     var playButton = document.getElementsByClassName("player__play")[0];
     var thrasher = document.getElementById("got-the-citadel-player-thrasher");
     document.getElementsByClassName("episode-info__title")[0].innerHTML = '<h1>' + name + '</h1>';
+    document.getElementsByClassName("episode-info__heading")[0].setAttribute('href', episode);
     var audioLength = 0;
     audio.oncanplaythrough = function(){
         audioLength = audio.duration;
@@ -58,3 +60,10 @@ loadJSON("http://interactive.guim.co.uk/docsdata-test/1rJsiJMlEqkSVbklFSKJBmwCYf
         document.getElementById("progress").setAttribute("stroke-dasharray", Math.floor(timer) + "," + "1000");
     }
 });
+
+setTimeout(function() {
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = 'section#the-citadel.fc-container.fc-container--thrasher.fc-container--has-toggle { background-color: #333;   overflow: visible !important; } section#the-citadel.fc-container.fc-container--thrasher.fc-container--has-toggle div.fc-container__inner, section#the-citadel.fc-container.fc-container--thrasher.fc-container--has-toggle div.facia-snap.facia-snap--default.fc-item.fc-item--audio.fc-item--has-image.fc-item--has-metadata.fc-item--is-commentable.fc-item--is-media-link.js-fc-item.js-snap.tone-media--item.fc-item--fluid-mobile.fc-item--fluid-tablet.js-snappable.facia-snap-embed.facia-snap-point--huge, div.facia-snap.facia-snap--default.fc-item.fc-item--audio.fc-item--has-image.fc-item--has-metadata.fc-item--is-commentable.fc-item--is-media-link.js-fc-item.js-snap.tone-media--item.fc-item--fluid-mobile.fc-item--fluid-tablet.js-snappable.facia-snap-embed.facia-snap-point--huge .fc-slice-wrapper {   overflow: visible !important; }';
+  document.body.appendChild(css);
+}, 2000);
