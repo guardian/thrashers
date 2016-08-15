db540c6a040a3e70f94377d89ccfa148a97b656c
Duarte Carrilho da Graça <duarte.carrilho.freelance@theguardian.com>
2016-08-15 12:19:15 +0100

    only load images that are visible in the day's thrasher

--- embeds/rio-2016-header/_source/main.js
+++ embeds/rio-2016-header/_source/main.js
@@ -5,9 +5,21 @@
   var today = new Date().getDate();
   $thrasher.setAttribute('data-day', today);
 
-
   $thrasher.addEventListener('click', function() {
     $thrasher.setAttribute('class', $thrasher.className+' easter')
+  });
+
+
+  // only load today's images
+  var $versions = $thrasher.querySelectorAll(".version");
+  $versions.forEach(function($version) {
+    // if version is visible
+    if ($version.offsetParent !== null) {
+      var $images = $version.querySelectorAll('img');
+      $images.forEach(function($img) {
+        $img.setAttribute('src', $img.getAttribute('data-src'));
+      });
+    }
   });
 
 })();
