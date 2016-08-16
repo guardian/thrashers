cbc3e448f5d3ce00b1a3360406cb53f1e1f0ba3c
Duarte Carrilho da Graça <duarte.carrilho.freelance@theguardian.com>
2016-08-16 12:19:52 +0100

    bugfix: forEach on NodeList

--- embeds/rio-2016-header/_source/main.js
+++ embeds/rio-2016-header/_source/main.js
@@ -1,10 +1,12 @@
-// set day as data-attr
-(function() {
 
-  var $thrasher = document.getElementById('rio-2016-header-inner');
+r(function(){
+
+  // set day as data-attr
   var today = new Date().getDate();
+  var $thrasher = document.getElementById('rio-2016-header-inner');
   $thrasher.setAttribute('data-day', today);
 
+  // easter egg on click
   $thrasher.addEventListener('click', function() {
     $thrasher.setAttribute('class', $thrasher.className+' easter')
   });
@@ -12,14 +14,17 @@
 
   // only load today's images
   var $versions = $thrasher.querySelectorAll(".version");
-  $versions.forEach(function($version) {
+  [].forEach.call($versions, function ($version) {
     // if version is visible
     if ($version.offsetParent !== null) {
       var $images = $version.querySelectorAll('img');
-      $images.forEach(function($img) {
+      [].forEach.call($images, function ($img) {
         $img.setAttribute('src', $img.getAttribute('data-src'));
       });
     }
   });
 
-})();
+
+});
+function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+NodeList.prototype.forEach = Array.prototype.forEach;
