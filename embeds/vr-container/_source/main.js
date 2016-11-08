cf4900c5c669da711621535b9692309d4284f083
Duarte Carrilho da Graça <duarte.carrilho.freelance@theguardian.com>
2016-11-08 17:19:09 +0000

    video plays

--- embeds/vr-container/_source/main.js
+++ embeds/vr-container/_source/main.js
@@ -1,3 +1,21 @@
 (function(){
-// https://uploads.guim.co.uk/2016/11/08/Underworld_tunnel_loop_abr800.mp4
+
+  injectVideo();
+
+  function injectVideo() {
+    console.log('runs')
+    // https://uploads.guim.co.uk/2016/11/08/Underworld_tunnel_loop_abr800.mp4
+    var videoTag = document.createElement('div');
+    videoTag.setAttribute('class', 'video');
+    videoTag.innerHTML = '<video poster="https://uploads.guim.co.uk/2016/11/03/in-play@1x.jpg" loop="true"><source src="https://uploads.guim.co.uk/2016/11/08/Underworld_tunnel_loop_abr800.mp4" type="video/mp4"></video>';
+
+    var underworldCard = document.querySelector('#vr-container .card.major');
+    underworldCard.appendChild(videoTag);
+
+    var video = underworldCard.querySelector('video');
+    video.load();
+    video.play();
+  }
+
+
 })();
