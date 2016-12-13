fd11a8213eaca08e26d2c3685ecb89489891b369
joshua-david <joshrobson4@gmail.com>
2016-12-13 18:02:00 +0000

    Expand/hide toggle JS added

--- embeds/docs-radical-brownies/_source/main.js
+++ embeds/docs-radical-brownies/_source/main.js
@@ -18,61 +18,45 @@ function loadJSON(path, success, error) {
 
 loadJSON("https://interactive.guim.co.uk/docsdata-test/1tZ4zY1P3k5jOtVR7a-e0DY_C7ND2_0zNvLjiff7-Rgw.json", function(data) {
     var backgroundImage = data.sheets.Sheet1[0].backgroundImage;
-
     var featuredPoster = data.sheets.Sheet1[0].featuredPoster;
     var featuredCopy = data.sheets.Sheet1[0].featuredCopy;
     var featuredLink = data.sheets.Sheet1[0].featuredLink;
-
     var secondPoster = data.sheets.Sheet1[0].secondPoster;
     var secondTitle = data.sheets.Sheet1[0].secondTitle;
     var secondCopy = data.sheets.Sheet1[0].secondCopy;
     var secondLink = data.sheets.Sheet1[0].secondLink;
-
     var thirdPoster = data.sheets.Sheet1[0].thirdPoster;
     var thirdTitle = data.sheets.Sheet1[0].thirdTitle;
     var thirdCopy = data.sheets.Sheet1[0].thirdCopy;
     var thirdLink = data.sheets.Sheet1[0].thirdLink;
 
+    document.getElementsByClassName("background-image")[0].style.backgroundImage = "url('" + backgroundImage + "')";
     document.getElementsByClassName("featured-doc-poster-source")[0].setAttribute("src", featuredPoster);
     document.getElementsByClassName("featured-doc-quote")[0].innerHTML = featuredCopy;
-    document.getElementsByClassName("featured-doc--cta")[0].setAttribute("src", featuredLink);
-
+    document.getElementsByClassName("featured-doc-cta")[0].setAttribute("src", featuredLink);
     document.getElementsByClassName("second-doc-poster-source")[0].setAttribute("src", secondPoster);
     document.getElementsByClassName("second-title")[0].innerHTML = secondTitle;
     document.getElementsByClassName("second-doc-quote")[0].innerHTML = secondCopy;
-    document.getElementsByClassName("second-doc--cta")[0].setAttribute("src", secondLink);
-
+    document.getElementsByClassName("second-doc-cta")[0].setAttribute("src", secondLink);
     document.getElementsByClassName("third-doc-poster-source")[0].setAttribute("src", thirdPoster);
     document.getElementsByClassName("third-title")[0].innerHTML = thirdTitle;
     document.getElementsByClassName("third-doc-quote")[0].innerHTML = thirdCopy;
-    document.getElementsByClassName("third-doc--cta")[0].setAttribute("src", thirdLink);
-
-    // var source = data.sheets.Sheet1[0].Trailer;
-    // var mp3 = data.sheets.Sheet1[0].Mp3;
-    // document.getElementsByClassName("audio")[0].setAttribute("src", source);
-    // document.getElementsByClassName("subscription__download")[0].setAttribute("href", mp3);
-    // var audio = document.getElementsByClassName("audio")[0];
-    // var playButton = document.getElementsByClassName("player__play")[0];
-    // var thrasher = document.getElementById("got-the-citadel-player-thrasher");
-    // document.getElementsByClassName("episode-info__title")[0].innerHTML = '<h1>' + name + '</h1>';
-    // document.getElementsByClassName("episode-info__heading")[0].setAttribute('href', episode);
+    document.getElementsByClassName("third-doc-cta")[0].setAttribute("src", thirdLink);
 });
 
-
-
-var buttonDown = document.querySelector('.toggle-button');
-var buttonUp = document.querySelector('.toggle-button-close');
-
-buttonDown.addEventListener('click', function(event) {
-    var expand = document.getElementById('expand');
+function showhide() {
+    var expand = document.getElementById("expand");
     var buttonMore = document.getElementById('buttonMore');
+    var buttonLess = document.getElementById('buttonLess');
 
-    if (expand.style.display == "none") {
-        // buttonMore.style.display = "none";
-        expand.style.display = "block";
+    if (expand.style.display !== "none") {
+        expand.style.display = "none";
+        buttonMore.style.visibility = "visible";
+        buttonLess.style.visibility = "hidden";
 
     } else {
-        // buttonMore.style.display = "block";
-        expand.style.display = "none";
+        expand.style.display = "block";
+        buttonMore.style.visibility = "hidden";
+        buttonLess.style.visibility = "visible";
     }
-});
+}
