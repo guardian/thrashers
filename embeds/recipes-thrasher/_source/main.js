9a60d869769b55d45418773ac0ad9847a61b10c6
blongden73 <hello@benlongdendesign.co.uk>
2017-04-18 10:31:10 +0100

    chefs headers

--- embeds/recipes-thrasher/_source/main.js
+++ embeds/recipes-thrasher/_source/main.js
@@ -1,11 +1,11 @@
 var timeofday;
 var breakfastImage = "https://media.guim.co.uk/fa5f16cebc0a18e1d63d50d6c850cbe01d4e546b/0_0_5635_4112/2000.jpg";
-var lunchImage = "https://media.guim.co.uk/b585c8780f7fdb62702c28665261991c72a3343f/0_433_6411_3847/2000.jpg";
+var lunchImage = "https://media.guim.co.uk/b1524b5e217e1a158feb7d9e85859ff11dd36f37/0_477_5671_3403/2000.jpg";
 var dinnerImage = "https://media.guim.co.uk/51f397f794a73ccd83b0bb79b982b4fff20c32bf/0_277_4666_2800/2000.jpg";
 var mainImage;
 var styleType;
 var dinnerStyle = "object-position: center 28%;";
-var lunchStyle = "object-position: center 54%;";
+var lunchStyle = "object-position: center 66%;";
 var breakfastStyle = "object-position: center 45%;";
 
 var headline = document.querySelector(".recipes-headline");
@@ -13,7 +13,7 @@ var background = document.querySelector(".recipes-background");
 
 var imageContainer = document.querySelector('.recipes');
 
-var date = new Date("July 21, 1983 08:15:00");
+var date = new Date();
 var currentHour = date.getHours();
 
 function initMealTime(){
@@ -27,7 +27,7 @@ function initMealTime(){
     timeofday = "lunch"
     mainImage = lunchImage;
     styleType = lunchStyle;
-    headlineClass = "lunch";
+    headlineClass = "breakfast";
   }
   if(currentHour >= 16 && currentHour < 23) {
     timeofday = "dinner";
