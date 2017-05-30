27b265f425e42589e93b2dceec239ecc54081d9a
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-05-30 12:32:19 +0100

    Add vizual test

---
+++ embeds/vizual-test/_source/main.js
@@ -0,0 +1,9 @@
+function addScript(src) {
+    var s = document.createElement('script');
+    s.setAttribute('src', src);
+    document.body.appendChild(s);
+}
+
+addScript(
+    'https://dnzy6u8jx448f.cloudfront.net/javascript/snippets/bL9Vi6JJ5gtuuOiKvziaTRzTvAkjIuMZ.js'
+);
