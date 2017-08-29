63684baf5ea79132f1d19337ef240c294bec0287
joshua-david <joshrobson4@gmail.com>
2017-08-29 14:23:18 +0100

    Football weekly update

--- embeds/football-weekly-august/_source/main.js
+++ embeds/football-weekly-august/_source/main.js
@@ -16,19 +16,19 @@ function loadJSON(path, success, error) {
     xhr.send();
 }
 
-loadJSON("http://interactive.guim.co.uk/docsdata-test/1pVaEqgTQNLxZL3TcvwkvvFCBNKjTmjwtgK_1NZjONrE.json", function(data) {
+loadJSON("https://interactive.guim.co.uk/docsdata-test/1pVaEqgTQNLxZL3TcvwkvvFCBNKjTmjwtgK_1NZjONrE.json", function(data) {
 
     var  source =  data.sheets.Sheet1[0].Trailer;
     var  mp3 = data.sheets.Sheet1[0].Mp3;
     var  itunes = data.sheets.Sheet1[0].Itunes;
     var  title = data.sheets.Sheet1[0].Title;
     var  subhead = data.sheets.Sheet1[0].Subhead;
-    var  full_episode_link = data.sheets.Sheet1[0].Link;
+    // var  full_episode_link = data.sheets.Sheet1[0].Link;
 
     document.getElementsByClassName("audio")[0].setAttribute("src", source);
     document.getElementsByClassName("subscription__download--link")[0].setAttribute("href", mp3);
     document.getElementsByClassName("subscription__itunes--link")[0].setAttribute("href", itunes);
-    document.getElementsByClassName("episode-info_full")[0].setAttribute("href", full_episode_link);
+    // document.getElementsByClassName("episode-info_full")[0].setAttribute("href", full_episode_link);
     var audio = document.getElementsByClassName("audio")[0];
     var playButton = document.getElementsByClassName("player__play")[0];
     var thrasher = document.getElementById("football-daily-euro-2016-podcast-thrasher");
@@ -43,7 +43,6 @@ loadJSON("http://interactive.guim.co.uk/docsdata-test/1pVaEqgTQNLxZL3TcvwkvvFCBN
 
         var secd = mind % 60;
         var seconds = Math.floor(secd);
-        document.getElementsByClassName("episode-info__time")[0].innerHTML = '<h2>' + minutes + ':' + seconds + '</h2>';
     }
     playButton.onclick = function(){
         if(audio.paused){
