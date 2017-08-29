a6a69fd99e1eec9d33bcd49ddc16d3d0bd2ed3ec
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-08-29 13:20:00 -0400

    update minute snap with new title

--- embeds/the-minute-snap/_source/main.js
+++ embeds/the-minute-snap/_source/main.js
@@ -1,5 +1,3 @@
-console.log('on load');
-
 if (window.XMLHttpRequest) {
    xhttp = new XMLHttpRequest();
 } else {
@@ -7,11 +5,13 @@ if (window.XMLHttpRequest) {
 }
 
 xhttp.overrideMimeType('text/xml');
-// xhttp.open("GET", 'https://www.theguardian.com/us-news/series/the-campaign-minute-2016', false);
+   xhttp.open("GET", 'https://www.theguardian.com/us-news/series/the-campaign-minute-2016/rss', false);
 xhttp.open("GET", 'https://localhost:8000/embeds/the-minute-snap/hashed/rss.349b8b71.xml', false);
 xhttp.send(null);
 xmlDoc = xhttp.responseXML;
 
+console.log(xmlDoc);
+
 var item = xmlDoc.getElementsByTagName("item")[0];
 
-document.getElementsByClassName('the-minute-snap__wrapper')[0].innerHTML = document.getElementsByClassName('the-minute-snap__wrapper')[0].innerHTML.replace('__HEADLINE__', item.getElementsByTagName("title")[0].childNodes[0]).replace('__IMAGE__', item.getElementsByTagName('media:content')[1].getAttribute('url'));
+document.getElementsByClassName('the-minute-snap__wrapper')[0].innerHTML = document.getElementsByClassName('the-minute-snap__wrapper')[0].innerHTML.replace('<!-- HEADLINE -->', item.getElementsByTagName("title")[0].childNodes[0].textContent.replace(' | The minute', '')).replace('cool-src', 'src=\'' + item.getElementsByTagName('media:content')[1].getAttribute('url') + '\'');
