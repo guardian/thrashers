40e07255829ec5421f1829a1240a4b1f23b16724
Andy Ball <andy_ball@32097.gnm.int>
2018-01-30 10:46:19 +1100

    Australian thrashers

--- embeds/the-reckoning/_source/slideshow.js
+++ embeds/the-reckoning/_source/slideshow.js
@@ -3,7 +3,7 @@
   var slideshow = (function() {
 
     document.getElementsByClassName('the-reckoning__wrapper')[0].onclick = function() {
-      window.open("https://www.theguardian.com/australia-news/audio/2017/dec/15/the-reckoning-part-2-david-marr-on-the-appalling-truth-revealed-at-the-child-sexual-abuse-commission", '_self');
+      window.open("https://www.theguardian.com/australia-news/audio/2017/dec/21/the-reckoning-part-3-david-marr-on-what-next-now-the-work-of-the-child-sexual-abuse-commission-is-over-podcast", '_self');
     };
 
   }());
