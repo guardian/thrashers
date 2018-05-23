001cb7aba4ce83d54a31746ba4d6c85d9a9cfc43
Duarte Carrilho da Graça <duarte.carrilho.casual@theguardian.com>
2018-05-23 10:11:17 +0100

    third shitteration of the missing something thrasher

--- embeds/missing-something/_source/main.js
+++ embeds/missing-something/_source/main.js
@@ -1,44 +1,44 @@
 (function() {
 
+
 	function setupUnreadCount() {
+		var typewriter = document.querySelector('.missing-something__typewriter');
 		
-		var marker = document.querySelector('.missing-something__marker');
-		var markerNum = marker.querySelector('.number');
-		
-		updateUnreadCount(marker, markerNum);
+		updateUnreadCount(typewriter);
 		window.addEventListener('scroll', function() {
-			updateUnreadCount(marker, markerNum);
+			updateUnreadCount(typewriter);
 		});
 
 	}
 	
-	function updateUnreadCount(marker, markerNum) {
-		var markerY = marker.getBoundingClientRect().y;
+	function updateUnreadCount(typewriter) {
+		var typewriterY = typewriter.getBoundingClientRect().y;
 		var windowHeight = window.innerHeight;
-		var onScreenRatio = (markerY/windowHeight);
-		var animationMultiplier = Math.max(0,Math.min(1,((onScreenRatio*2)-0.75)));
-		
-		var maxEmails = 12;
-		var emails = parseInt(maxEmails*animationMultiplier);
-		
-		if (emails > 0) {
-			marker.classList.add('on');
-			markerNum.setAttribute('data-num', emails);
+		var onScreenRatio = (typewriterY/windowHeight);
+		var animationMultiplier = 1-Math.max(0,Math.min(1,((onScreenRatio*onScreenRatio)-0.2)));
+
+		if (animationMultiplier < 1) {
+			typewriter.classList.add('blink');
 		} else {
-			marker.classList.remove('on');
+			typewriter.classList.remove('blink');
 		}
+		
+		var fullText = typewriter.getAttribute('data-text');
+		var displayCharacters = animationMultiplier * fullText.length;
+		var currentText = fullText.substring(0, displayCharacters);
+		typewriter.innerText = currentText;
 	}
 
 	function trackLoad() {
 		window.guardian.ophan.record({
-			component: 'missing something thrasher v2 : load',
+			component: 'missing something thrasher v3 : load',
 			value: 1
 		});
 	}
 
 	function checkExists(startThrasherFunction) {
 		var checkInterval = setInterval(function() {
-			if (document.querySelector('.missing-something__marker')) {
+			if (document.querySelector('.missing-something__typewriter')) {
 				startThrasherFunction();
 				clearInterval(checkInterval);
 			}
