0b156fb8435df2b4fc18c2994e1a466c71eedbf6
joshua-david <joshrobson4@gmail.com>
2018-11-08 17:16:00 +0000

    final tweaks

--- embeds/million-thrasher/_source/main.js
+++ embeds/million-thrasher/_source/main.js
@@ -1,88 +1,126 @@
-try {
-  // Checks if a contriubtor or not
-  const isCont = document.cookie.split(';').map(a => a.trim()).map(a => a.split('=', 2)).filter(a => ['gu_paying_member', 'gu_digital_subscriber', 'gu_recurring_contributor', 'gu.contributions.contrib-timestamp'].includes(a[0])).filter(a => a[1] != 'false').length != 0
+(function() {
 
-  // Checks if in UK
-  const isUK = ["GB"].includes(JSON.parse(localStorage.getItem('gu.geolocation')).value);
-  const isAU = ["AU"].includes(JSON.parse(localStorage.getItem('gu.geolocation')).value);
+  var isCont;
 
-  const thanks = document.getElementById("million-wrap-thanks");
-  const sell = document.getElementById("million-wrap-sell");
-  const subcribeButtonSell = document.getElementById("uk-only-sell");
-  const subcribeButtonThanks = document.getElementById("uk-only-thanks");
-  const dots = document.getElementById("dots-one");
-  const dotsTwo = document.getElementById("dots-two");
+  var isUK;
+  var isAU;
 
-  const allSell = document.getElementById("all-sell");
-  const allThanks = document.getElementById("all-thanks");
-  const ausOnlySell = document.getElementById("aus-only-sell");
-  const ausOnlyThanks = document.getElementById("aus-only-thanks");
+  var thanks;
+  var sell;
+  var subcribeButtonSell;
+  var subcribeButtonThanks;
+  var dots;
+  var dotsTwo;
 
-  // If a contributor, show appropriate thrahser
-  if (isCont == true) {
-    thanks.classList.add("show");
-    console.log('Is a contributor');
+  var allSell;
+  var allThanks;
+  var ausOnlySell;
+  var ausOnlyThanks;
 
-  } else {
-    sell.classList.add("show");
-    console.log('Is not a contributor');
+
+  function loadThrasherElements() {
+      // Checks if a contriubtor or not
+      isCont = document.cookie.split(';').map(a => a.trim()).map(a => a.split('=', 2)).filter(a => ['gu_paying_member', 'gu_digital_subscriber', 'gu_recurring_contributor', 'gu.contributions.contrib-timestamp'].includes(a[0])).filter(a => a[1] != 'false').length != 0
+
+      // Checks if in UK
+      isUK = ["GB"].includes(JSON.parse(localStorage.getItem('gu.geolocation')).value);
+      isAU = ["AU"].includes(JSON.parse(localStorage.getItem('gu.geolocation')).value);
+
+      thanks = document.getElementById("million-wrap-thanks");
+      sell = document.getElementById("million-wrap-sell");
+      subcribeButtonSell = document.getElementById("uk-only-sell");
+      subcribeButtonThanks = document.getElementById("uk-only-thanks");
+      dots = document.getElementById("dots-one");
+      dotsTwo = document.getElementById("dots-two");
+
+      allSell = document.getElementById("all-sell");
+      allThanks = document.getElementById("all-thanks");
+      ausOnlySell = document.getElementById("aus-only-sell");
+      ausOnlyThanks = document.getElementById("aus-only-thanks");
+
+      // If a contributor, show appropriate thrahser
+      if (isCont == true) {
+        thanks.classList.add("show");
+        console.log('Is a contributor');
+
+      } else {
+        sell.classList.add("show");
+        console.log('Is not a contributor');
+      }
+
+      // If oustside UK hide the subscibe button
+      if (isUK == false) {
+        subcribeButtonSell.classList.add("hide");
+        subcribeButtonThanks.classList.add("hide");
+        console.log('Is outside the UK');
+      }
+
+      // just for debugging
+      else if (isUK == true) {
+        console.log('Is in the UK');
+      }
+
+      // If in AU hide the subscibe button and show the correct copy
+      if (isAU == false) {
+        // subcribeButtonSell.classList.add("hide");
+        // subcribeButtonThanks.classList.add("hide");
+
+        // hide the rest of the world copy
+        allSell.classList.add("hide");
+        allThanks.classList.add("hide");
+
+        // show the aus specific copy
+        ausOnlySell.classList.add("show");
+        ausOnlyThanks.classList.add("show");
+
+        console.log('Is in AUS');
+
+      } else {
+        // hide the world copy
+        allSell.classList.add("show");
+        allThanks.classList.add("show");
+
+        // show the aus copy
+        ausOnlySell.classList.add("hide");
+        ausOnlyThanks.classList.add("hide");
+      }
   }
 
-  // If not in UK hide the subscibe button
-  if (isUK == false) {
-    subcribeButtonSell.classList.add("hide");
-    subcribeButtonThanks.classList.add("hide");
-    console.log('Is outside the UK');
-  }
+    // Wait for the thrasher to be in view to start animations
+    function playThrasher() {
+      var thrasher = document.querySelector('.million-thrasher__wrapper');
 
-  // just for debugging
-  else if (isUK == true) {
-    console.log('Is in the UK');
-  }
+      window.addEventListener('scroll', function() {
+        var thrasherY = thrasher.getBoundingClientRect().y;
+              var peekSize = 200;
 
-  // If in AU hide the subscibe button and show the correct copy
-  if (isAU == true) {
-    subcribeButtonSell.classList.add("hide");
-    subcribeButtonThanks.classList.add("hide");
+        var windowHeight = window.innerHeight;
+              if ((thrasherY - windowHeight + peekSize) < 0) {
+                  thrasher.classList.add('playing');
+                  dots.classList.add('show');
+                  dotsTwo.classList.add('show');
+              }
+      });
+    }
 
-    // hide the rest of the world copy
-    allSell.classList.add("hide");
-    allThanks.classList.add("hide");
+    function checkExists(startThrasherFunction) {
+      var checkInterval = setInterval(function() {
+        if (document.querySelector('#million-wrap-thanks')) {
+          startThrasherFunction();
+          clearInterval(checkInterval);
+        }
+      }, 100);
+    }
 
-    // show the aus specific copy
-    ausOnlySell.classList.add("show");
-    ausOnlyThanks.classList.add("show");
+    function trackLoad() {
+      window.guardian.ophan.record({
+        component: 'Thrasher : Million campaign : Load',
+        value: 1
+      });
+    }
 
-    console.log('Is in AUS');
+    checkExists(loadThrasherElements);
+    checkExists(playThrasher);
+    checkExists(trackLoad);
 
-  } else {
-    // hide the world copy
-    allSell.classList.add("show");
-    allThanks.classList.add("show");
-
-    // show the aus copy
-    ausOnlySell.classList.add("hide");
-    ausOnlyThanks.classList.add("hide");
-  }
-
-  // Wait for the thrasher to be in view to start animations
-  function playThrasher() {
-    const thrasher = document.querySelector('.million-thrasher__wrapper');
-
-    window.addEventListener('scroll', function() {
-      const thrasherY = thrasher.getBoundingClientRect().y;
-            const peekSize = 200;
-
-      const windowHeight = window.innerHeight;
-            if ((thrasherY - windowHeight + peekSize) < 0) {
-                thrasher.classList.add('playing');
-                dots.classList.add('show');
-                dotsTwo.classList.add('show');
-            }
-    });
-  }
-  playThrasher();
-
-} catch (error) {
-  console.error(error);
-}
+})();
