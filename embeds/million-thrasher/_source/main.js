d028e88d301b36032df7459027381e6c893f94d5
joshua-david <joshrobson4@gmail.com>
2018-11-08 13:32:02 +0000

    AUS logic added in

--- embeds/million-thrasher/_source/main.js
+++ embeds/million-thrasher/_source/main.js
@@ -4,29 +4,65 @@ try {
 
   // Checks if in UK
   const isUK = ["GB"].includes(JSON.parse(localStorage.getItem('gu.geolocation')).value);
+  const isAU = ["AU"].includes(JSON.parse(localStorage.getItem('gu.geolocation')).value);
 
   const thanks = document.getElementById("million-wrap-thanks");
   const sell = document.getElementById("million-wrap-sell");
-  const ukOnly = document.getElementById("uk-only");
+  const subcribeButtonSell = document.getElementById("uk-only-sell");
+  const subcribeButtonThanks = document.getElementById("uk-only-thanks");
   const dots = document.getElementById("dots-one");
   const dotsTwo = document.getElementById("dots-two");
+
+  const allSell = document.getElementById("all-sell");
+  const allThanks = document.getElementById("all-thanks");
+  const ausOnlySell = document.getElementById("aus-only-sell");
+  const ausOnlyThanks = document.getElementById("aus-only-thanks");
 
   // If a contributor, show appropriate thrahser
   if (isCont == true) {
     thanks.classList.add("show");
-    sell.classList.add("hide");
     console.log('Is a contributor');
 
   } else {
-    thanks.classList.add("hide");
     sell.classList.add("show");
     console.log('Is not a contributor');
   }
 
-  // If in UK, if so hide the subscibe button
+  // If not in UK hide the subscibe button
   if (isUK == false) {
-    ukOnly.classList.add("hide");
+    subcribeButtonSell.classList.add("hide");
+    subcribeButtonThanks.classList.add("hide");
     console.log('Is outside the UK');
+  }
+
+  // just for debugging
+  else if (isUK == true) {
+    console.log('Is in the UK');
+  }
+
+  // If in AU hide the subscibe button and show the correct copy
+  if (isAU == true) {
+    subcribeButtonSell.classList.add("hide");
+    subcribeButtonThanks.classList.add("hide");
+
+    // hide the rest of the world copy
+    allSell.classList.add("hide");
+    allThanks.classList.add("hide");
+
+    // show the aus specific copy
+    ausOnlySell.classList.add("show");
+    ausOnlyThanks.classList.add("show");
+
+    console.log('Is in AUS');
+
+  } else {
+    // hide the world copy
+    allSell.classList.add("show");
+    allThanks.classList.add("show");
+
+    // show the aus copy
+    ausOnlySell.classList.add("hide");
+    ausOnlyThanks.classList.add("hide");
   }
 
   // Wait for the thrasher to be in view to start animations
@@ -40,19 +76,12 @@ try {
       const windowHeight = window.innerHeight;
             if ((thrasherY - windowHeight + peekSize) < 0) {
                 thrasher.classList.add('playing');
-
-                // wait 4 sec for 2nd frame to show dots
-                setTimeout(function(){
-                  dots.classList.add('show');
-                  dotsTwo.classList.add('show');
-                }, 4000);
-
-
+                dots.classList.add('show');
+                dotsTwo.classList.add('show');
             }
     });
   }
   playThrasher();
-
 
 } catch (error) {
   console.error(error);
