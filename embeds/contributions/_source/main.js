279e070aca163b67c36d2419395607506a64741a
Jesse Yuen <jesse.yuen.casual@theguardian.com>
2019-02-15 14:46:12 +0000

    conditional js added

--- embeds/contributions/_source/main.js
+++ embeds/contributions/_source/main.js
@@ -1,28 +1,35 @@
 (function() {
 
-	var target = '.contribution-moment';
-	// var trigger = document.querySelector('.contribution-moment__heading');
-
-	// trigger.addEventListener('click', toggleClass);
-
-	// function toggleClass(){
-	// 	console.log('clicked');
-		
-	// 	if (target.classList.contains('is-animated')) {
-	// 		target.classList.remove('is-animated');
-	// 	} else {
-	// 		target.classList.add('is-animated');
-	// 	}
-	// }
+	var target = '.contribution-moment'
 
 	function checkExists(startThrasherFunction) {
-		console.log('running');
-			var checkInterval = setInterval(function() {
-					if (document.querySelector(target)) {
-							startThrasherFunction();
-							clearInterval(checkInterval);
-					}
-			}, 100);
+		var checkInterval = setInterval(function() {
+				if (document.querySelector(target)) {
+						startThrasherFunction();
+						clearInterval(checkInterval);
+				}
+		}, 100);
+	}
+	
+	// If a contributor, show appropriate thrahser content
+	function toggleThrasherElements() {
+		isCont = document.cookie.split(';').map(a => a.trim()).map(a => a.split('=', 2)).filter(a => ['gu_paying_member', 'gu_digital_subscriber', 'gu_recurring_contributor', 'gu.contributions.contrib-timestamp'].includes(a[0])).filter(a => a[1] != 'false').length != 0
+
+		var thanks = document.getElementsByClassName('thanks');
+    var sell = document.getElementsByClassName('sell');
+
+		if (isCont === true) {
+			for(var i = 0; i < sell.length; i++){
+				sell[i].classList.add('hide');
+			}
+			console.log('Is a contributor');
+
+		} else {
+			for(var i = 0; i < thanks.length; i++){
+				thanks[i].classList.add('hide');
+			}
+			console.log('Is not a contributor');
+		}
 	}
 
 	function playThrasher() {
@@ -42,6 +49,7 @@
 	}
 
 	checkExists(playThrasher);
-	toggleClass();
+	toggleThrasherElements();
+	// toggleClass();
 
 })();
