3fd4c62203a82584395fe019d9b56fc6825b4524
Jesse Yuen <jesse.yuen.casual@theguardian.com>
2019-02-18 12:36:27 +0000

    Localisation copy and js tests added

--- embeds/contributions/_source/main.js
+++ embeds/contributions/_source/main.js
@@ -15,23 +15,74 @@
 	function toggleThrasherElements() {
 		isCont = document.cookie.split(';').map(a => a.trim()).map(a => a.split('=', 2)).filter(a => ['gu_paying_member', 'gu_digital_subscriber', 'gu_recurring_contributor', 'gu.contributions.contrib-timestamp'].includes(a[0])).filter(a => a[1] != 'false').length != 0
 
-		var thanks = document.getElementsByClassName('thanks');
-    var sell = document.getElementsByClassName('sell');
+		// Checks if in UK
+		isUK = ["GB"].includes(JSON.parse(localStorage.getItem('gu.geolocation')).value);
+		isAU = ["AU"].includes(JSON.parse(localStorage.getItem('gu.geolocation')).value);
 
-		if (isCont === true) {
-			for(var i = 0; i < sell.length; i++){
+		var thanks = document.getElementsByClassName('thanks');
+    	var sell = document.getElementsByClassName('sell');
+    	var row = document.getElementsByClassName('row');
+
+		// Is a contributer	
+		// if (isCont === true)  {
+		// 	for(var i = 0; i < sell.length; i++){
+		// 		sell[i].classList.add('hide');
+		// 	}
+
+		// 	for(var i = 0; i < row.length; i++){
+		// 		row[i].classList.add('hide');
+		// 	}
+		// 	// console.log('Is a contributor');
+
+		// // Is NOT a contributer	
+		// } else {
+		// 	for(var i = 0; i < thanks.length; i++){
+		// 		thanks[i].classList.add('hide');
+		// 	}
+		// 	// console.log('Is not a contributor');
+		// }
+
+		// If oustside UK ...
+		if (isUK === false) {
+			console.log('Is outside the UK');
+			for(var i = 0; i < sell.length; i++) {
 				sell[i].classList.add('hide');
 			}
-			// console.log('Is a contributor');
+			for(var i = 0; i < thanks.length; i++) {
+				thanks[i].classList.add('hide');
+			}
+			
+		// Is inside UK ...	
+		} if (isUK === true) {
+			console.log('Is in the UK');
+			for(var i = 0; i < row.length; i++) {
+				row[i].classList.add('hide');
+			}
 
-		} else {
+		// Is NOT a contributer	
+		} if (isCont === false) {
+			console.log('Is not a contributer');
 			for(var i = 0; i < thanks.length; i++){
 				thanks[i].classList.add('hide');
 			}
-			// console.log('Is not a contributor');
+		
+		// if a contributer and lives anywhere	
+		} if ( ((isUK === true) || (isUK === false)) && (isCont === true)) {
+			console.log('match all three');
+			
+			for(var i = 0; i < sell.length; i++) {
+				sell[i].classList.add('hide');
+			}
+			
+			for(var i = 0; i < thanks.length; i++) {
+				thanks[i].classList.remove('hide');
+			}
+
+			for(var i = 0; i < row.length; i++) {
+				row[i].classList.add('hide');
+			}
 		}
 	}
-
 	// function playThrasher() {
 
 	// 	var thrasher = document.querySelector(target);
@@ -50,29 +101,33 @@
 	
 	function playThrasher() {
 
-    var target = '.contribution-moment';
-    var thrasher = document.querySelector(target);
-    var thrasherY = thrasher.getBoundingClientRect().y;
-    var peekSize = 150;
-    var windowHeight = window.innerHeight;
+		console.log('fired');
 
-    if ((thrasherY - windowHeight + peekSize) < 0) {
-        thrasher.classList.add('is-animated');
-    }
+		var target = '.contribution-moment';
+		var thrasher = document.querySelector(target);
+		var thrasherY = thrasher.getBoundingClientRect().y;
+		var peekSize = 150;
+		var windowHeight = window.innerHeight;
 
-    window.addEventListener('scroll', function() {
-        var thrasherY = thrasher.getBoundingClientRect().y;
-        var peekSize = 150;
-        var windowHeight = window.innerHeight;
+		if ((thrasherY - windowHeight + peekSize) < 0) {
+			thrasher.classList.add('is-animated');
+		}
 
-        if ((thrasherY - windowHeight + peekSize) < 0) {
-            thrasher.classList.add('is-animated');
-        }
-    });
-  }
+		window.addEventListener('scroll', function() {
+					console.log('scrolling');
 
-	playThrasher();
-	// checkExists(playThrasher);
+			var thrasherY = thrasher.getBoundingClientRect().y;
+			var peekSize = 150;
+			var windowHeight = window.innerHeight;
+
+			if ((thrasherY - windowHeight + peekSize) < 0) {
+				thrasher.classList.add('is-animated');
+			}
+		});
+  	}
+
+	// playThrasher();
+	checkExists(playThrasher);
 	toggleThrasherElements();
 	// toggleClass();
 
