6e4c61c487548ab14754d6515b3cc720f78f9345
Mariot Chauvin <mariot.chauvin@gmail.com>
2019-10-07 18:54:53 +0100

    remove logging

--- embeds/podcasts-archive/_source/main.js
+++ embeds/podcasts-archive/_source/main.js
@@ -1,6 +1,5 @@
 ;(function ( global ) {
   //Move the container outside of the standard list element
-  console.log("RUNNING")
   let sliceWrapper = document.querySelector('#from-the-archive .fc-slice-wrapper');
   let directoryWrapper = document.querySelector('#from-the-archive .podcasts-archive__wrapper');
   sliceWrapper.appendChild(directoryWrapper);
