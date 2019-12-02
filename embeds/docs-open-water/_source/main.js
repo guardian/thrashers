53fc63752d0e51491bdf7810859468a592dc2245
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2019-12-02 11:12:08 +0100

    adds comment to explain vertical scroll JS

--- embeds/docs-open-water/_source/main.js
+++ embeds/docs-open-water/_source/main.js
@@ -12,6 +12,11 @@
 		var thrasher = document.querySelector('.docs-open-water__wrapper');
 		var sea = thrasher.querySelector('.sea-group')
 		var r = onScreenRatio(thrasher);
+		// r = 1 when the the thrasher at the bottom or further
+		// r = 0.5 when the thrasher is in the middle
+		// r = 0 when the thrasher hits the top of the screen
+		// this uses the vertical middle of the thrasher as a
+		// reference point (i.e 140px down the top on a 280px thrasher)
 
 		if (r > 0.65) {
 			sea.dataset.step = '1';
