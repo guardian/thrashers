1500c31aa53aeff3bd8d2b431df8510c4567487c
Juweek Adolphe <jadolphe23@gmail.com>
2019-12-13 18:20:17 -0500

    added dec 31 thrasher

--- embeds/us-new-year-fundraiser/_source/main.js
+++ embeds/us-new-year-fundraiser/_source/main.js
@@ -1,36 +1,6 @@
-var newYearCount = 60
-target,
-interval;
 var count = 0;
 var goal;
 var total;
-
-
-function animateCountdown() {
-    target = getCountdownTarget();
-
-    if (interval === undefined) {
-        interval = setInterval(increaseNewYearCounter, 1);
-    }
-};
-
-function getCountdownTarget() {
-    var oneDay = 24*60*60*1000;
-    var currentDate = new Date();
-    var targetDate = new Date('Jan 1, 2020');
-
-    return Math.floor(Math.abs((targetDate.getTime() - currentDate.getTime())/(oneDay))) + 1;
-};
-
-function increaseNewYearCounter() {
-    newYearCount -= 1;
-
-    document.getElementsByClassName('new-year__countdown__number')[0].innerHTML = newYearCount;
-
-    if (newYearCount <= target) {
-        document.getElementsByClassName('new-year__countdown__number')[0].innerHTML = target;
-    }
-};
 
 function percentageTotalAsNegative() {
     var percentage = total / goal * 100 - 100;
@@ -41,7 +11,7 @@ function percentageTotalAsNegative() {
 };
 
 function animateBar() {
-    var progressBarElement = document.querySelector('.us-end-of-year-2019__progress-complete');
+    var progressBarElement = document.querySelector('.us-new-year-fundraiser__progress-complete');
 
     if (progressBarElement && progressBarElement instanceof HTMLElement) {
         progressBarElement.style.transform = 'translateX(' + percentageTotalAsNegative() + '%)';
@@ -50,7 +20,7 @@ function animateBar() {
 
 function increaseCounter() {
     count += Math.floor(total / 100);
-    var counterElement = document.querySelector('.us-end-of-year-2019__count');
+    var counterElement = document.querySelector('.us-new-year-fundraiser__count');
 
     if (counterElement && counterElement instanceof HTMLElement) {
         counterElement.innerHTML = '$' + count.toLocaleString();
@@ -66,6 +36,7 @@ function fetchDataAndAnimate() {
     fetch('https://support.theguardian.com/ticker.json').then(function (resp) {
         return resp.json();
     }).then(function (data) {
+        console.log('yoyoyo');
         total = parseInt(data.total, 10);
         goal = parseInt(data.goal, 10);
 
@@ -77,4 +48,3 @@ function fetchDataAndAnimate() {
 };
 
 fetchDataAndAnimate();
-animateCountdown();
