e3775ff03768ba72785f238db20661533f2464fb
Alvin Chang <alvinschang@gmail.com>
2021-12-13 16:06:11 -0500

    update new years US thrashers

---
+++ embeds/us-eoy-2021-pre-jan/_source/main.js
@@ -0,0 +1,59 @@
+var count = 0;
+var goal;
+var total;
+var completedGoal = false;
+
+function percentageTotalAsNegative() {
+    var percentage = total / goal * 100 - 100;
+    if (percentage > 0) {
+        percentage = 0;
+    }
+    return percentage;
+};
+
+function animateBar() {
+    var progressBarElement = document.querySelector('.us-eoy-2021-pre-jan__wrapper .us-end-of-year-2019__progress-complete');
+
+    if (progressBarElement && progressBarElement instanceof HTMLElement) {
+        progressBarElement.style.transform = 'translateX(' + percentageTotalAsNegative() + '%)';
+    }
+};
+
+function increaseCounter() {
+    count += Math.floor(total / 100);
+    var counterElement = document.querySelector('.us-eoy-2021-pre-jan__wrapper .us-end-of-year-2019__count span');
+
+    if (counterElement && counterElement instanceof HTMLElement) {
+        counterElement.innerHTML = '$' + count.toLocaleString();
+        if (count >= total) {
+            counterElement.innerHTML = '$' + total.toLocaleString();
+        } else {
+            window.requestAnimationFrame(increaseCounter);
+        }
+    }
+};
+
+function fetchDataAndAnimate() {
+    fetch('https://support.theguardian.com/ticker.json').then(function (resp) {
+        return resp.json();
+    }).then(function (data) {
+        total = parseInt(data.total, 10);
+        goal = parseInt(data.goal, 10);
+        if (total >= 1250000) {
+            if (total > 1300000) {
+                total = 1300000;
+            }
+            goal = 1388889;
+            document.getElementById('us-end-of-year-2019__our_goal').innerHTML = "<span class='us-end-of-year-2019__red'>But it's not too late to give!</span>";
+            document.querySelector('.us-end-of-year-2019__total').classList.add("us-end-of-year-2019__goal_reached");
+            completedGoal = true;
+        }
+
+        window.setTimeout(function () {
+            window.requestAnimationFrame(increaseCounter);
+            animateBar();
+        }, 500);
+    });
+};
+
+fetchDataAndAnimate();
