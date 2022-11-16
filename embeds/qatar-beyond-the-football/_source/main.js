49329173d54f9ac6d93cee31076df6cba75eea60
Harry Fischer <harry.fischer@guardian.co.uk>
2022-11-16 16:07:41 +0000

    adding qatar atom

---
+++ embeds/qatar-beyond-the-football/_source/main.js
@@ -0,0 +1,92 @@
+function findTargetContainer(container) {
+    var prev = container.previousElementSibling;
+    var classes = prev.classList;
+    if (classes.contains('fc-container--thrasher') || classes.contains('fc-container--commercial') || classes.contains('fc-container--video') || classes.contains('fc-container__mpu--mobile')) {
+        var target = findTargetContainer(prev);
+    } else {
+        var target = prev;
+    }
+    return target;
+}
+
+function closest(el, selector) {
+    var matchesFn;
+
+    // find vendor prefix
+    ['matches', 'webkitMatchesSelector', 'mozMatchesSelector', 'msMatchesSelector', 'oMatchesSelector'].some(function (fn) {
+        if (typeof document.body[fn] == 'function') {
+            matchesFn = fn;
+            return true;
+        }
+        return false;
+    })
+
+    var parent;
+
+    // traverse parents
+    while (el) {
+        parent = el.parentElement;
+        if (parent && parent[matchesFn](selector)) {
+            return parent;
+        }
+        el = parent;
+    }
+
+    return null;
+}
+
+
+function thrasherMain() {
+    // find this thrasher
+    var thrasher = document.querySelector('.qatar-nugget');
+
+    // the thrasher's section tag
+    var thrasherContainer = closest(thrasher, 'section');
+
+    // section where the treat should go
+    var targetContainer = findTargetContainer(thrasherContainer);
+
+    // get thrasher innerHTML
+    var thrasherWrap = document.createElement('div');
+    thrasherWrap.appendChild(thrasher.cloneNode(true));
+    var thrasherHTML = thrasherWrap.innerHTML;
+
+    if (targetContainer.offsetHeight < 260) {
+        return null
+    }
+
+    // clone thrasher into newTreat
+    var newTreat = document.createElement('li');
+    newTreat.classList.add('treats__list-item');
+    newTreat.innerHTML = thrasherHTML;
+
+    // find target container's treats
+    // or make it if it doesnt' exist
+    var containerTreats = targetContainer.querySelector('.treats__container');
+    if (!containerTreats) {
+        var newTreatList = document.createElement('ul');
+        newTreatList.classList.add('treats__container');
+        targetContainer.querySelector('.fc-container__inner').appendChild(newTreatList);
+        containerTreats = targetContainer.querySelector('.treats__container');
+    }
+
+    // inject on top if there are treats, at bottom otherwise
+    var firstTreat = containerTreats.querySelector('li:first-child');
+    if (firstTreat) {
+        containerTreats.insertBefore(newTreat, firstTreat);
+        firstTreat.classList.add('treat-separator');
+    } else {
+        containerTreats.appendChild(newTreat);
+    }
+}
+
+function checkExists(startThrasherFunction) {
+    var checkInterval = setInterval(function () {
+        if (document.querySelector('.qatar-nugget')) {
+            startThrasherFunction();
+            clearInterval(checkInterval);
+        }
+    }, 2000);
+}
+
+checkExists(thrasherMain);
